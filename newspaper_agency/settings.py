85faaa470a1851a249e3a3b6cd33500318bea689
Alexander <alexzubkoff123@gmail.com>
2022-11-14 01:02:37 +0200

    Added domain name host to ALLOWED_HOSTS

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "newspaper-agency-alex-zubkov-mate.onrender.com",
+]
 
 
 # Application definition
